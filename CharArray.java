000bbbaf3ed63e0a28d4513699c85fcd317a647d
Pranali Shendage <159568977+pranali-shendage@users.noreply.github.com>
2025-12-13 17:45:35 +0530

    Add files via upload

---
+++ CharArray.java
@@ -0,0 +1,11 @@
+public class CharArray{
+public static void main(String []x){
+
+char []arr={'N','P','O','E','R','S','Y','T'};
+
+for(int i=0;i<arr.length;i++)
+ {
+  System.out.println(arr[i]+" "+(int)arr[i]+" ");
+  }
+ }
+}
