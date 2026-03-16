f43f6c6f78af0656124b6b65e204527f4ac4ee87
Pranali Shendage <159568977+pranali-shendage@users.noreply.github.com>
2026-03-16 22:53:53 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,11 @@
+
+class Test {
+    public static void main(String[] args) {
+        int[] a={1,2,3,4};
+        for(int i=0;i<a.length;i++)
+            a[i]=a[i]*i;
+        System.out.print(a[0]+a[1]+a[2]+a[3]);
+    }
+}
+
+
